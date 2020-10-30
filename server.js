274eae3cbec65893d8f5cce171760808213ee8ac
Nithin Deepkumar <nithindeepkumar@gmail.com>
2020-10-30 20:22:21 +0530

    add express json parser (now included with express) and express-validator to users post route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect to DB
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('API running'));
