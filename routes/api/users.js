1e7ebff9c915d9bfaa095caa415078f5a2a78014
Nithin Deepkumar <nithindeepkumar@gmail.com>
2020-10-31 12:52:08 +0530

    add Gravatar and JWT(Json web token) to users.js POST route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const jwtSecret = config.get('jwtSecret');
+
+//db imports
+const User = require('../../models/User');
 
 //Route POST api/users
 
@@ -14,14 +22,47 @@ router.post(
 			'Please enter a password with 6 or more characters'
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
 
-		res.send('User Route');
+		const { name, email, password } = req.body;
+
+		try {
+			//See if user already exists
+			let user = await User.findOne({ email });
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exists' }] });
+			}
+			//Get users gravatar
+			let avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+			user = new User({ name, email, avatar, password });
+
+			//Encrypt password
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
+
+			//Return Json WebToken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(payload, jwtSecret, { expiresIn: 360000 }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
