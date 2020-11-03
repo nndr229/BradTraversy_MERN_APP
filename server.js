fb56aa9efc00976dd62c1cca9288abe0d6df72c1
Nithin Deepkumar <nithindeepkumar@gmail.com>
2020-11-03 16:07:57 +0530

    add react startup through create react app

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(mongoSanitize());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API running'));
 
