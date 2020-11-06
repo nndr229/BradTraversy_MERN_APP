110f305b7c73139d9b28281f03ddbc3810eab5c2
Nithin Deepkumar <nithindeepkumar@gmail.com>
2020-11-06 09:50:21 +0530

    Add Reducer for state management, set up reducer for Alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios'; 
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-export const Register = () => {
+const Register = ({ setAlert }) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -16,24 +18,9 @@ export const Register = () => {
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		if (password !== password2) {
-			console.log('Passwords do not match!');
+			setAlert('Passwords do not match!', 'danger');
 		} else {
 			console.log('Success');
-			// const newUser = { name, email, password };
-			// try {
-			// 	const config = {
-			// 		headers: {
-			// 			'Content-Type': 'application/json',
-			// 		},
-			// 	};
-
-			// 	const body = JSON.stringify(newUser);
-
-			// 	const res = await axios.post('/api/users', body, config);
-			// 	console.log(res.data);
-			// } catch (err) {
-			// 	console.log(err.response.data);
-			// }
 		}
 	};
 
@@ -97,3 +84,9 @@ export const Register = () => {
 		</Fragment>
 	);
 };
+
+Register.propTypes = {
+	setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
